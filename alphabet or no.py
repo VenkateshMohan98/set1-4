1048448e436d886ac30c6906fd3dfc431e84a0d1
VenkateshMohan98 <51222591+VenkateshMohan98@users.noreply.github.com>
2019-07-12 16:30:03 +1000

    Create alphabet or no.py

---
+++ alphabet or no.py
@@ -0,0 +1,6 @@
+v=input()
+aph=['a','b','c','d','e','f','g','h','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if v in aph:
+    print("Alphabet")
+else:
+    print("No")
